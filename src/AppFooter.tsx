e892ad38db18f44138d323b1f7a833acfacf7d16
extrange <29305375+extrange@users.noreply.github.com>
2023-07-30 15:08:37 +0000

    minor: default to focus, merge overdue/due counts

--- src/AppFooter.tsx
+++ src/AppFooter.tsx
@@ -1,56 +1,28 @@
 import { Center, Footer, Text } from "@mantine/core";
-import { differenceInCalendarDays } from "date-fns";
-import React, { useMemo } from "react";
+import React from "react";
 import { NetworkOverlay } from "./NetworkOverlay";
 import { useAppStore } from "./appStore";
 import { useRerenderDaily } from "./useRerenderDaily";
+import { Todo } from "./useSyncedStore";
+
+const getDueString = (dueTodos: Todo[]) => {
+  const numDueTodos = dueTodos.length;
+  if (!numDueTodos) return "";
+
+  return `${dueTodos.length} todo${numDueTodos !== 1 ? "s" : ""} due`;
+};
 
 export const AppFooter = React.memo(() => {
   // will shallow compare improve rendering time?
   const dueTodos = useAppStore((state) => state.dueTodos);
 
-  const time = useRerenderDaily();
-
-  const todosDueToday = useMemo(
-    () =>
-      dueTodos.filter(
-        (t) => differenceInCalendarDays(time, Date.parse(t.dueDate)) === 0
-      ).length,
-    [dueTodos, time]
-  );
-
-  const todosOverdue = useMemo(
-    () =>
-      dueTodos.filter(
-        (t) => differenceInCalendarDays(time, Date.parse(t.dueDate)) > 0
-      ).length,
-    [dueTodos, time]
-  );
-
-  const dueString = useMemo(() => {
-    if (!todosDueToday && !todosOverdue) return "No todos due";
-
-    const todosDueTodayStr = `${todosDueToday} todo${
-      todosDueToday !== 1 ? "s" : ""
-    } due today`;
-
-    const todosOverdueStr = `${todosOverdue} todo${
-      todosOverdue !== 1 ? "s" : ""
-    } overdue`;
-
-    if (todosDueToday && todosOverdue)
-      return `${todosDueTodayStr} (${todosOverdue} overdue)`;
-
-    if (!todosDueToday && todosOverdue) return todosOverdueStr;
-
-    if (todosDueToday && !todosOverdue) return todosDueTodayStr;
-  }, [todosDueToday, todosOverdue]);
+  useRerenderDaily();
 
   return (
     <Footer height={30}>
       <Center h="100%">
         <Text c="dimmed" fz="sm">
-          {dueString}
+          {getDueString(dueTodos)}
         </Text>
       </Center>
       <NetworkOverlay />
--- src/ListProvider.tsx
+++ src/ListProvider.tsx
@@ -2,9 +2,10 @@ import React, { useMemo, useState } from "react";
 import { IListContext, ListContext } from "./ListContext";
 
 export const ListProvider = ({ children }: React.PropsWithChildren) => {
+  // Default to 'Focus'
   const [currentList, setCurrentList] = useState<
     IListContext["currentList"] | undefined
-  >();
+  >("focus");
 
   const providerValue = useMemo(
     () => ({ currentList, setCurrentList }),
